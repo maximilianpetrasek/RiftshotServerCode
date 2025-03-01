59df4a1e41aef7bb7c4fbf2682e325cb96a620e0
Lettucechips22 <maxwolfpath@gmail.com>
2025-02-28 20:15:03 -0700

    Initial commit
    The server code at the time of Febuary 25th, 2025.

---
+++ Riftshot Server/Player.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace Riftshot_Server
+{
+    class Player
+    {
+        public int id;
+        public string username;
+
+        public Vector3 position;
+        public Quaternion rotation;
+
+        private float moveSpeed = 5f / Constants.TICKS_PER_SEC;
+        //private bool[] inputs;
+
+        public Player(int _id, string _username, Vector3 _spawnPosition)
+        {
+            id = _id;
+            username = _username;
+            position = _spawnPosition;
+            rotation = Quaternion.Identity;
+
+            //inputs = new bool[4];
+        }
+
+        public void Update()
+        {
+            
+
+            Move(position);
+        }
+
+        private void Move(Vector3 _position)
+        {
+            
+
+            ServerSend.PlayerPosition(this);
+            ServerSend.PlayerRotation(this);
+        }
+
+        public void SetInput(Vector3 _position, Quaternion _rotation)
+        {
+            position = _position;
+            rotation = _rotation;
+        }
+
+        public void SetAnims(string AnimName, Vector3 ComboValues)
+        {
+            ServerSend.PlayerAnimation(this, AnimName, ComboValues);
+        }
+
+        public void SetOutfit(Vector3 DataPing)
+        {
+            ServerSend.PingPong(this, DataPing);
+        }
+    }
+}
---
+++ Riftshot Server/ServerHandle.cs
@@ -0,0 +1,54 @@
+﻿using Riftshot_Server;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Riftshot_Server
+{
+    class ServerHandle
+    {
+        public static void WelcomeReceived(int _fromClient, Packet _packet)
+        {
+            int _clientIdCheck = _packet.ReadInt();
+            string _username = _packet.ReadString();
+
+            Console.WriteLine($"{Server.clients[_fromClient].tcp.socket.Client.RemoteEndPoint} connected successfully and is now player {_fromClient}.");
+            if (_fromClient != _clientIdCheck)
+            {
+                Console.WriteLine($"Player \"{_username}\" (ID: {_fromClient}) has assumed the wrong client ID ({_clientIdCheck})!");
+            }
+            Server.clients[_fromClient].SendIntoGame(_username);
+        }
+
+        public static void PlayerMovement(int _fromClient, Packet _packet)
+        {
+            //bool[] _inputs = new bool[_packet.ReadInt()];
+            //for (int i = 0; i < _inputs.Length; i++)
+            //{
+            //    _inputs[i] = _packet.ReadBool();
+            //}
+            Vector3 _position = _packet.ReadVector3();
+            Quaternion _rotation = _packet.ReadQuaternion();
+
+            Server.clients[_fromClient].player.SetInput(_position, _rotation);
+        }
+
+        public static void PlayerAnimation(int _fromClient, Packet _packet)
+        {
+            string AName = _packet.ReadString();
+            Vector3 ComboValue = _packet.ReadVector3();
+
+            Server.clients[_fromClient].player.SetAnims(AName, ComboValue);
+        }
+
+        public static void CustomIntPingPong(int _fromClient ,Packet _packet)
+        {
+            Vector3 WEREGONNA = _packet.ReadVector3();
+            if(WEREGONNA.Z == 0)
+            {
+                Server.clients[_fromClient].player.SetOutfit(WEREGONNA);
+            }
+        }
+    }
+}
